0f5de7265286ec1d6c0d6fae50298ca534f42765
Sawan Rughoobeer <sawan.rughoobeer@gmail.com>
2026-08-18 08:12:07 +0000

    change materialization for raw, staging and reporting models

--- models/raw/raw_customer.sql
+++ models/raw/raw_customer.sql
@@ -4,4 +4,4 @@
     )
 }}
 select * from 
-RAW.GLOBALMART.CUSTOMER
+{{ source('globalmart', 'customer') }}
--- models/raw/raw_orders.sql
+++ models/raw/raw_orders.sql
@@ -4,4 +4,4 @@
     )
 }}
 select * from 
-RAW.GLOBALMART.ORDERS
+{{ source('globalmart', 'orders') }}
